a46ce44284dd04606fa850b44745068b5d72249f
Jeremy-Mossa <jeremy.mossa@gmail.com>
2024-04-09 20:55:19 -0700

    string variables and println practice

--- first_steps.go
+++ first_steps.go
@@ -4,12 +4,15 @@ import "fmt"
 
 func main() {
 
-/* A multi-line comment
-Looks similar to other
-languages' style of commenting
-*/
+    /* A multi-line comment
+    Looks similar to other
+    languages' style of commenting
+    */
 
-fmt.Println("Hello, from lang of Go, 2nd iteration")
+    // A single-line comment
 
-// A single-line comment
+
+    var str1 = "today is 9 April 2024"
+    fmt.Printf("Hello, from lang of Go, 2nd iteration\n"+str1+"\n")
+
 }
