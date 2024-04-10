d518872ec542b84134a8a5cf9e2738daff066473
Jeremy-Mossa <jeremy.mossa@gmail.com>
2024-04-09 20:39:45 -0700

    rename file to first_steps. hello no longer a meaningful filename.

---
+++ first_steps.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+/* A multi-line comment
+Looks similar to other
+languages' style of commenting
+*/
+
+fmt.Println("Hello, from lang of Go, 2nd iteration")
+
+// A single-line comment
+}
