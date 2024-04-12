a507d269779814dcf434a0ed31962b1ab2803822
Jeremy-Mossa <jeremy.mossa@gmail.com>
2024-04-11 20:53:27 -0700

    go by example exercises

--- 9-slices.go
+++ 9-slices.go
@@ -2,7 +2,7 @@ package main
 
 import (
   "fmt"
-  //"slices"
+  "slices"
 )
 
 func main() {
@@ -12,4 +12,48 @@ func main() {
 
   s = make([]string, 3)
   fmt.Println("emp:", s, "len:", len(s), "cap:", cap(s))
+
+  s[0] = "a" 
+  s[1] = "b"
+  s[2] = "c"
+
+  fmt.Println("set:", s)
+  fmt.Println("get:", s[2])
+
+  fmt.Println("len:", len(s))
+
+  s = append(s, "d")
+  s = append(s, "e", "f")
+  fmt.Println("apd:", s)
+
+  c := make([]string, len(s))
+  copy(c, s)
+  fmt.Println("cpy:", c)
+
+  l := s[1:5]
+  fmt.Println("sl1:", l)
+
+  l = s[:5]
+  fmt.Println("sl2:", l)
+
+  l = s[3:]
+  fmt.Println("sl3:", l)
+
+  t := []string{"x", "y", "z"}
+  t2 := []string{"x", "y", "z"}
+  fmt.Println(t); fmt.Println(t2)
+
+  if slices.Equal(t, t2) {
+    fmt.Println("t == t2")
+  }
+  
+  twoD := make([][]int, 3)
+  for i := 0; i < 3; i++ {
+    innerLen := i + 1
+    twoD[i] = make([]int, innerLen)
+    for j := 0; j < innerLen; j++ {
+      twoD[i][j] = i + j
+    }
+  }
+  fmt.Println("twoD", twoD)
 }
