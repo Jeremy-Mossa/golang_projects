421b82324236aa6ce3aeb85fc02d0fd660fdaf3e
Jeremy-Mossa <jeremy.mossa@gmail.com>
2024-04-29 18:32:57 -0700

    continue go by example

--- 32-timeouts.go
+++ 32-timeouts.go
@@ -8,7 +8,15 @@ import (
 var pln = fmt.Println
 
 func main() {
+  c1 := make(chan string, 1)
+    go func() {
+      time.Sleep(2 * time.Second)
+      c1 <- "result 1"
+    }()
 
-
-
+  c2 := make(chan string, 1)
+    go func() {
+      time.Sleep(2 * time.Second)
+      c2 <- "result 2"
+    }()
 }
