b21866b1ce34a2a8a0db3438f1ae722899ecfe6d
Jeremy-Mossa <jeremy.mossa@gmail.com>
2024-05-04 14:47:17 -0700

    continue go by example

--- 32-timeouts.go
+++ 32-timeouts.go
@@ -14,9 +14,24 @@ func main() {
       c1 <- "result 1"
     }()
 
+  select {
+  case res := <-c1:
+    pln(res)
+  case <-time.After(1 * time.Second):
+    pln("timeout 1")
+  }
+
   c2 := make(chan string, 1)
     go func() {
       time.Sleep(2 * time.Second)
       c2 <- "result 2"
     }()
+
+  select {
+  case res := <-c2:
+    pln(res)
+  case <-time.After(3 * time.Second):
+    pln("timeout 2")
+  }
+
 }
